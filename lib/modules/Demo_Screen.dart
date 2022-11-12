fed94c8b0105a2a8c1b3df54f5dfa933c17d0e6d
MAin2001 <aminmahmoud843@gmail.com>
2022-11-12 15:32:59 +0200

    first commit

---
+++ lib/modules/Demo_Screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DemoScreen extends StatelessWidget {
+  const DemoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello Demo'),
+      ),
+    );
+  }
+}
